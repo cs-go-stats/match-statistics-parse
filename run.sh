055082b03875db4a123cb8e958e91bb07c7401f3
Viktor Liutianskyi <born.to.run.unix@gmail.com>
2020-01-08 14:49:04 +0200

    Replaced run.sh file to call centralized automation scripts.

---
+++ run.sh
@@ -0,0 +1,16 @@
+category=services
+service=match-statistics-parse
+version=0.1.0
+
+cd ./../../automation_scripts
+
+#<project_context> <repository>
+./verify.sh $category $service
+
+#<project_context> <project_name> <package_version> <pack_nuget> <pack_objects> <pack_docker>
+./push.sh $category $service $version no no yes
+
+if [ "$#" -eq "1" ]; then
+	#<service_name> <version>
+	./deploy.sh $service $version
+fi
